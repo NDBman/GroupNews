ba251cd49db80d2a07541c4584fe22e8f4d2f42e
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-05 16:31:54 +0200

    Inital commit

---
+++ src/main/java/com/epam/internship/GroupNewsApplication.java
@@ -0,0 +1,12 @@
+package com.epam.internship;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupNewsApplication.class, args);
+	}
+}
