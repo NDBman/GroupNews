e3ff252cb268c10f426d4c84d445fc6bb677bc36
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-11 15:38:23 +0200

    UserController uses the service layer.

--- GroupNews-web/src/main/java/com/epam/internship/controller/UserController.java
+++ GroupNews-web/src/main/java/com/epam/internship/controller/UserController.java
@@ -3,11 +3,13 @@ package com.epam.internship.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.epam.internship.UserService;
 import com.epam.internship.entity.UserEntity;
 
 @RestController
@@ -16,13 +18,13 @@ public class UserController {
 
 	private List<UserEntity> users = new ArrayList<>();
 	
-	public UserController() {
-		users.add(UserEntity.builder().name("Mr. Brown").email("brown@test.com").build());
-		users.add(UserEntity.builder().name("Mr. Green").email("green@test.com").build());
-	}
+	@Autowired
+	private UserService userService;
 	
 	@RequestMapping(method= RequestMethod.GET, produces= MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public List<UserEntity> getUsers(){
+		users.add(userService.createUser("Mr. Brown", "brown@test.com"));
+		users.add(userService.createUser("Mr. Green", "green@test.com"));
 		return users;
 	}
 }
