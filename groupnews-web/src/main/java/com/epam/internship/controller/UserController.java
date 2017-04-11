5717eecc1189cd3f0a29a38eef3b1c2023b75312
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-11 16:41:35 +0200

    Deleted request method specification because it's default.

--- groupnews-web/src/main/java/com/epam/internship/controller/UserController.java
+++ groupnews-web/src/main/java/com/epam/internship/controller/UserController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.epam.internship.UserService;
@@ -21,7 +20,7 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@RequestMapping(produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public List<User> getUsers() {
 		users.add(userService.createUser("Mr. Brown", "brown@test.com"));
 		users.add(userService.createUser("Mr. Green", "green@test.com"));
