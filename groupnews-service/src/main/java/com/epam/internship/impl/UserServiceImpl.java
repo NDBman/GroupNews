d0d56ba4ac704c3b7936e059ea8193a1d7c048d8
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-20 01:14:17 +0200

    Added Test cases for UserServiceImpl class.

--- groupnews-service/src/main/java/com/epam/internship/impl/UserServiceImpl.java
+++ groupnews-service/src/main/java/com/epam/internship/impl/UserServiceImpl.java
@@ -3,32 +3,38 @@ package com.epam.internship.impl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.stereotype.Service;
 
-import com.epam.internship.UserConverter;
 import com.epam.internship.UserService;
 import com.epam.internship.dto.User;
+import com.epam.internship.entity.UserEntity;
 import com.epam.internship.repo.UserRepository;
 
 @Service
 public class UserServiceImpl implements UserService {
-	
+
 	@Autowired
 	private UserRepository userRepository;
-	
-	private UserConverter userConverter = new UserConverter();
+
+	@Autowired
+	private ConversionService conversionService;
 
 	@Override
 	public User createUser(String name, String email) {
 		User newUser = User.builder().name(name).email(email).build();
-		userRepository.save(userConverter.convertUser(newUser));
+		userRepository.save(conversionService.convert(newUser, UserEntity.class));
 		return newUser;
 	}
 
+	@SuppressWarnings("unchecked")
 	@Override
 	public List<User> getAllUsers() {
-		return userConverter.convertUserEntities(userRepository.findAll());
-		
+		return (List<User>) conversionService.convert(userRepository.findAll(),
+				TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(UserEntity.class)),
+				TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(User.class)));
+
 	}
 
 }
--- groupnews-service/src/test/java/com/epam/internship/UserServiceTest.java
+++ groupnews-service/src/test/java/com/epam/internship/UserServiceTest.java
@@ -11,6 +11,8 @@ import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.springframework.core.convert.ConversionService;
+import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.epam.internship.dto.User;
@@ -20,7 +22,7 @@ import com.epam.internship.repo.UserRepository;
 
 @RunWith(SpringRunner.class)
 public class UserServiceTest {
-	
+
 	private final String USER_NAME_1 = "Mr. Brown";
 	private final String USER_NAME_2 = "Mr. Green";
 	private final String USER_EMAIL_1 = "brown@test.com";
@@ -28,43 +30,46 @@ public class UserServiceTest {
 
 	@Mock
 	private UserRepository userRepository;
-	
+
+	@Mock
+	private ConversionService conversionService;
+
 	private UserEntity userEntity1;
 	private UserEntity userEntity2;
-	
+
 	private User user1;
 	private User user2;
-	
+
 	@InjectMocks
-	private UserServiceImpl userServiceImpl;
-	
-	@Mock
-	private UserConverter userConverter;
-	
+	private UserServiceImpl systemUnderTest;
+
 	@Before
-	public void setUp(){
+	public void setUp() {
 		userEntity1 = UserEntity.builder().name(USER_NAME_1).email(USER_EMAIL_1).build();
 		userEntity2 = UserEntity.builder().name(USER_NAME_2).email(USER_EMAIL_2).build();
 		user1 = User.builder().name(USER_NAME_1).email(USER_EMAIL_1).build();
 		user2 = User.builder().name(USER_NAME_2).email(USER_EMAIL_2).build();
 		Mockito.when(userRepository.findAll()).thenReturn(Arrays.asList(userEntity1, userEntity2));
-		Mockito.when(userConverter.convertUserEntities(userRepository.findAll())).thenReturn(Arrays.asList(user1,user2));
-		Mockito.when(userConverter.convertUser(user1)).thenReturn(userEntity1);
 		Mockito.when(userRepository.save(userEntity1)).thenReturn(userEntity1);
+		Mockito.when(conversionService.convert(userRepository.findAll(),
+				TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(UserEntity.class)),
+				TypeDescriptor.collection(List.class, TypeDescriptor.valueOf(User.class))))
+				.thenReturn(Arrays.asList(user1, user2));
 	}
-	
+
 	@Test
-	public void shoudlReturnAllUsers(){
-		List<User> users = userServiceImpl.getAllUsers();
+	public void shoudlReturnAllUsers() {
+		System.out.println(systemUnderTest.getAllUsers());
+		List<User> users = systemUnderTest.getAllUsers();
 		assertEquals(users.get(0).getName(), USER_NAME_1);
 		assertEquals(users.get(0).getEmail(), USER_EMAIL_1);
 		assertEquals(users.get(1).getName(), USER_NAME_2);
 		assertEquals(users.get(1).getEmail(), USER_EMAIL_2);
 	}
-	
+
 	@Test
-	public void shouldCreateUser(){
-		User user = userServiceImpl.createUser(USER_NAME_1, USER_EMAIL_1);
+	public void shouldCreateUser() {
+		User user = systemUnderTest.createUser(USER_NAME_1, USER_EMAIL_1);
 		assertEquals(USER_NAME_1, user.getName());
 		assertEquals(USER_EMAIL_1, user.getEmail());
 	}
