e750762d5a854d6d3aa7b61621f9cc4212eb5ba1
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-20 20:50:36 +0200

    Added service annotation.

--- groupnews-service/src/main/java/com/epam/internship/converter/UserConverter.java
+++ groupnews-service/src/main/java/com/epam/internship/converter/UserConverter.java
@@ -1,10 +1,12 @@
 package com.epam.internship.converter;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Service;
 
 import com.epam.internship.dto.User;
 import com.epam.internship.entity.UserEntity;
 
+@Service
 public class UserConverter implements Converter<User, UserEntity> {
 
 	@Override
--- groupnews-service/src/main/java/com/epam/internship/converter/UserEntityConverter.java
+++ groupnews-service/src/main/java/com/epam/internship/converter/UserEntityConverter.java
@@ -1,10 +1,12 @@
 package com.epam.internship.converter;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Service;
 
 import com.epam.internship.dto.User;
 import com.epam.internship.entity.UserEntity;
 
+@Service
 public class UserEntityConverter implements Converter<UserEntity, User> {
 
 	@Override
