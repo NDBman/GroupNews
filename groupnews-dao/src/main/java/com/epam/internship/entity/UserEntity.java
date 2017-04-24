1f181c6d8cbfc51c69ccb3099eab46ba3c5cb1a1
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-24 19:59:56 +0200

    Reformat.

--- groupnews-dao/src/main/java/com/epam/internship/entity/UserEntity.java
+++ groupnews-dao/src/main/java/com/epam/internship/entity/UserEntity.java
@@ -17,14 +17,14 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name="users")
+@Table(name = "users")
 public class UserEntity {
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@Column(nullable=false)
+	@Column(nullable = false)
 	private String name;
-	@Column(unique= true)
+	@Column(unique = true)
 	private String email;
 }
