f40ca046f6350f5b66001248970e7db2fbb70a54
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-27 20:15:58 +0200

    Changed Runner to SpringRunner in IT and Removed Mockito's runner from converter tests.

---
+++ groupnews-service/src/test/java/com/epam/internship/UserConverterTest.java
@@ -0,0 +1,28 @@
+package com.epam.internship;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.epam.internship.converter.UserConverter;
+import com.epam.internship.dto.User;
+import com.epam.internship.entity.UserEntity;;
+
+public class UserConverterTest {
+
+	private final String USER_NAME = "Mr. Brown";
+	private final String USER_EMAIL = "brown@email.com";
+
+	private UserConverter systemUnderTest = new UserConverter();
+
+	@Test
+	public void shouldReturnUserEntityWithSameFieldValues() {
+		// Given
+		User user = User.builder().name(USER_NAME).email(USER_EMAIL).build();
+		// When
+		UserEntity userEntity = systemUnderTest.convert(user);
+		// Then
+		assertEquals(USER_NAME, userEntity.getName());
+		assertEquals(USER_EMAIL, userEntity.getEmail());
+	}
+}
---
+++ groupnews-service/src/test/java/com/epam/internship/UserEntityConverterTest.java
@@ -0,0 +1,28 @@
+package com.epam.internship;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.epam.internship.converter.UserEntityConverter;
+import com.epam.internship.dto.User;
+import com.epam.internship.entity.UserEntity;
+
+public class UserEntityConverterTest {
+
+	private final String USER_NAME = "Mr. Brown";
+	private final String USER_EMAIL = "brown@email.com";
+
+	private UserEntityConverter systemUnderTest = new UserEntityConverter();
+
+	@Test
+	public void shouldReturnUserEntityWithSameFieldValues() {
+		// Given
+		UserEntity userEntity = UserEntity.builder().name(USER_NAME).email(USER_EMAIL).build();
+		// When
+		User user = systemUnderTest.convert(userEntity);
+		// Then
+		assertEquals(USER_NAME, user.getName());
+		assertEquals(USER_EMAIL, user.getEmail());
+	}
+}
