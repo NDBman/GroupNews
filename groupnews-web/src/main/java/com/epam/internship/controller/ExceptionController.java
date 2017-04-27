818882c207046a3031c3356de4eff86aee510a7f
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-27 13:18:52 +0200

    Created NotFound Exception handler.

--- groupnews-web/src/main/java/com/epam/internship/controller/ExceptionController.java
+++ groupnews-web/src/main/java/com/epam/internship/controller/ExceptionController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import javassist.NotFoundException;
+
 @ControllerAdvice
 public class ExceptionController {
 
@@ -19,5 +21,11 @@ public class ExceptionController {
 	@ExceptionHandler(DataIntegrityViolationException.class)
 	public void handleConflict() {
 
+	}
+	
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NotFoundException.class)
+	public void handleNotFound(){
+		
 	}
 }
