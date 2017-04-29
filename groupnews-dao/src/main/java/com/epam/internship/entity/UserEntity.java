2868d6eea16b1e6d0e7080d402651984bf5cb153
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-29 16:55:35 +0200

    Deleted NoArgs and AllArgs const as it seems it isn't used.

--- groupnews-dao/src/main/java/com/epam/internship/entity/UserEntity.java
+++ groupnews-dao/src/main/java/com/epam/internship/entity/UserEntity.java
@@ -7,16 +7,12 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "users")
 public class UserEntity {
 
