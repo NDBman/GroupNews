7522b7079a3100df48880832eed8a52ffdabc211
Nándor Szabo <n.szabo@bssn-software.de>
2017-04-29 19:19:47 +0200

    Added NoArgs and AllArgs constructors.

--- groupnews-dao/src/main/java/com/epam/internship/entity/GroupEntity.java
+++ groupnews-dao/src/main/java/com/epam/internship/entity/GroupEntity.java
@@ -5,20 +5,26 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @Entity
 @Table(name="groups")
+@AllArgsConstructor
+@NoArgsConstructor
 public class GroupEntity {
 
 	@Id
 	@GeneratedValue(strategy= GenerationType.IDENTITY)
 	private Long id;
+	@OneToOne(targetEntity=UserEntity.class)
 	private UserEntity createdBy;
 	@Column(nullable = false, length = 70)
 	private String title;
--- groupnews-dao/src/main/java/com/epam/internship/entity/UserEntity.java
+++ groupnews-dao/src/main/java/com/epam/internship/entity/UserEntity.java
@@ -7,12 +7,16 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class UserEntity {
 
