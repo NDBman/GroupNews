1b09a5acf0b8691efe088e8b818f6dd88fcc9a02
Nándor Szabo <n.szabo@bssn-software.de>
2017-05-03 00:28:43 +0200

    Implemented createGroup service.

--- groupnews-service/src/main/java/com/epam/internship/impl/GroupServiceImpl.java
+++ groupnews-service/src/main/java/com/epam/internship/impl/GroupServiceImpl.java
@@ -2,16 +2,18 @@ package com.epam.internship.impl;
 
 import javax.transaction.Transactional;
 
+import org.apache.commons.lang3.Validate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.convert.ConversionService;
 import org.springframework.stereotype.Service;
 
 import com.epam.internship.GroupService;
-import com.epam.internship.UserService;
 import com.epam.internship.dto.Group;
 import com.epam.internship.entity.GroupEntity;
 import com.epam.internship.entity.UserEntity;
+import com.epam.internship.exception.UserDoesNotExistsException;
 import com.epam.internship.repo.GroupRepository;
+import com.epam.internship.repo.UserRepository;
 
 @Service
 @Transactional
@@ -21,14 +23,22 @@ public class GroupServiceImpl implements GroupService {
 	private GroupRepository groupRepository;
 
 	@Autowired
-	private UserService userService;
+	private UserRepository userRepository;
 
 	@Autowired
 	private ConversionService conversionService;
 
 	@Override
 	public Group createGoup(Long userId, String title, String description) {
-		UserEntity userEntity = conversionService.convert(userService.getUserById(userId), UserEntity.class);
+		UserEntity userEntity = userRepository.findOne(userId);
+		if (userEntity == null) {
+			throw new UserDoesNotExistsException();
+		}
+
+		Validate.notBlank(title);
+		Validate.inclusiveBetween(1, 70, title.length());
+		Validate.inclusiveBetween(1, 2000, description.length());
+
 		GroupEntity groupEntity = GroupEntity.builder().title(title).description(description).createdBy(userEntity)
 				.build();
 		groupEntity = groupRepository.save(groupEntity);
