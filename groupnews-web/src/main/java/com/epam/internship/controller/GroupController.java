ece0be84afa1e28f565a5c4b9110b78b01feaad5
Nándor Szabo <n.szabo@bssn-software.de>
2017-05-07 16:18:31 +0200

    Made end point to list users's groups.

--- groupnews-web/src/main/java/com/epam/internship/controller/GroupController.java
+++ groupnews-web/src/main/java/com/epam/internship/controller/GroupController.java
@@ -1,6 +1,9 @@
 package com.epam.internship.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,5 +24,10 @@ public class GroupController {
 	public Group createGroup(@PathVariable("userId") Long userId, @RequestParam String title,
 			@RequestParam String description) {
 		return groupService.createGoup(userId, title, description);
+	}
+	
+	@GetMapping("users/{userId}/groups")
+	public List<Group> getAllGroupsBelongingToUser(@PathVariable("userId")Long userId){
+		return groupService.listGroupsBelongingToUser(userId);
 	}
 }
