943878654a02f7fc389f28ae1c7c2e09a693882f
Nándor Szabo <n.szabo@bssn-software.de>
2017-05-07 16:19:00 +0200

    Made tests for new end point.

--- groupnews-web/src/test/java/com/epam/internship/GroupControllerTest.java
+++ groupnews-web/src/test/java/com/epam/internship/GroupControllerTest.java
@@ -1,9 +1,12 @@
 package com.epam.internship;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.util.Arrays;
 
 import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.Test;
@@ -22,21 +25,25 @@ import com.epam.internship.exception.UserDoesNotExistsException;
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers = { GroupController.class })
 public class GroupControllerTest {
-	
+
 	@Autowired
 	private MockMvc mockMvc;
-	
+
 	@MockBean
 	private GroupService groupService;
-	
+
 	private User user1;
-	
+
 	private final String GROUP_TITLE = "Group title";
 	private final String GROUP_DESCRIPION = "Group Desc";
-	
+
 	private final Long USER_ID_1 = 1L;
 	private final String USER_NAME_1 = "Mr Brown";
 	private final String USER_EMAIL_1 = "brown@test.com";
+
+	private final Long GROUP_ID_2 = 2L;
+	private final String GROUP_TITLE_2 = "Group title 2";
+	private final String GROUP_DESCRIPION_2 = "Group Desc 2";
 
 	@Test
 	public void shoudlReturnGroupWithGivenFields() throws Exception {
@@ -91,5 +98,40 @@ public class GroupControllerTest {
 		mockMvc.perform(post("/users/1/groups").param("title", GROUP_TITLE).param("description", longDesc))
 				// Then
 				.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	public void shouldReturnListOfGroupsBelongingToUser() throws Exception {
+		// Given
+		Group group1 = Group.builder().id(1L).title(GROUP_TITLE).description(GROUP_DESCRIPION).createdBy(user1).build();
+		Group group2 = Group.builder().id(GROUP_ID_2).title(GROUP_TITLE_2).description(GROUP_DESCRIPION_2)
+				.createdBy(user1).build();
+		when(groupService.listGroupsBelongingToUser(USER_ID_1)).thenReturn(Arrays.asList(group1, group2));
+		// When
+		mockMvc.perform(get("/users/1/groups"))
+				// Then
+				// .andExpect(jsonPath("$[:1].id").value(group1.getId()))
+				// .andExpect(jsonPath("$[:1].createdBy.id").value(group1.getCreatedBy().getId()))
+				.andExpect(jsonPath("$[:1].createdBy.name").value(group1.getCreatedBy().getName()))
+				.andExpect(jsonPath("$[:1].createdBy.email").value(group1.getCreatedBy().getEmail()))
+				.andExpect(jsonPath("$[:1].title").value(group1.getTitle()))
+				.andExpect(jsonPath("$[:1].description").value(group1.getDescription()))
+				// .andExpect(jsonPath("$[1:2].id").value(group2.getId()))
+				// .andExpect(jsonPath("$[1:2].createdBy.id").value(group2.getCreatedBy().getId()))
+				.andExpect(jsonPath("$[1:2].createdBy.name").value(group2.getCreatedBy().getName()))
+				.andExpect(jsonPath("$[1:2].createdBy.email").value(group2.getCreatedBy().getEmail()))
+				.andExpect(jsonPath("$[1:2].title").value(group2.getTitle()))
+				.andExpect(jsonPath("$[1:2].description").value(group2.getDescription())).andExpect(status().isOk());
+	}
+
+	@Test
+	public void shouldReturnNotFound() throws Exception {
+		// Given
+		when(groupService.listGroupsBelongingToUser(10L)).thenThrow(new UserDoesNotExistsException());
+		// When
+		mockMvc.perform(get("/users/10/groups"))
+				// Then
+				.andExpect(status().isNotFound());
+
 	}
 }
