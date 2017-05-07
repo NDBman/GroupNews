a4998cd55f1292a94cdabf2f62a7d65c7e1e16bf
Nándor Szabo <n.szabo@bssn-software.de>
2017-05-07 16:16:34 +0200

    Created custom query for getting all groups belonging to user.

--- groupnews-dao/src/main/java/com/epam/internship/repo/GroupRepository.java
+++ groupnews-dao/src/main/java/com/epam/internship/repo/GroupRepository.java
@@ -1,11 +1,15 @@
 package com.epam.internship.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.epam.internship.entity.GroupEntity;
+import com.epam.internship.entity.UserEntity;
 
 @Repository
 public interface GroupRepository extends JpaRepository<GroupEntity, Long> {
 
+	List<GroupEntity> findByCreatedBy(UserEntity userEntity);
 }
