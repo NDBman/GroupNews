a1ee8e82c2356b245e951d842fd6e60d6f43377f
Nándor Szabo <n.szabo@bssn-software.de>
2017-05-08 13:07:46 +0200

    Changed relation to Many to One.

--- groupnews-dao/src/main/java/com/epam/internship/entity/GroupEntity.java
+++ groupnews-dao/src/main/java/com/epam/internship/entity/GroupEntity.java
@@ -6,7 +6,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToOne;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import lombok.AllArgsConstructor;
@@ -25,7 +25,7 @@ public class GroupEntity {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	@OneToOne(cascade = CascadeType.ALL)
+	@ManyToOne(cascade = CascadeType.ALL)
 	private UserEntity createdBy;
 	@Column(nullable = false, length = 70)
 	private String title;
