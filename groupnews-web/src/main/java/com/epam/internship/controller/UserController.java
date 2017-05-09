6868f1c3e4112b4b575ea2d07353c660cf2c4323
Nándor Szabo <n.szabo@bssn-software.de>
2017-05-09 22:31:50 +0200

    Configured logging and logged an end point calling.

--- groupnews-web/src/main/java/com/epam/internship/controller/UserController.java
+++ groupnews-web/src/main/java/com/epam/internship/controller/UserController.java
@@ -2,6 +2,8 @@ package com.epam.internship.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,12 +20,15 @@ import com.epam.internship.dto.User;
 @RestController
 @RequestMapping("/")
 public class UserController {
+	
+	final static Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	@Autowired
 	private UserService userService;
 
 	@GetMapping("users")
 	public List<User> getUsers() {
+		logger.debug("Called: getUsers");
 		return userService.getAllUsers();
 	}
 
