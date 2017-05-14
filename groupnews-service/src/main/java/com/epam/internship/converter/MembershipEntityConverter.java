9b1512398ebf177cc7153ea14dcc1a9c669d0646
Nándor Szabo <n.szabo@bssn-software.de>
2017-05-15 00:25:21 +0200

    Added service annotation.

--- groupnews-service/src/main/java/com/epam/internship/converter/MembershipEntityConverter.java
+++ groupnews-service/src/main/java/com/epam/internship/converter/MembershipEntityConverter.java
@@ -1,10 +1,12 @@
 package com.epam.internship.converter;
 
 import org.springframework.core.convert.converter.Converter;
+import org.springframework.stereotype.Service;
 
 import com.epam.internship.dto.Membership;
 import com.epam.internship.entity.MembershipEntity;
 
+@Service
 public class MembershipEntityConverter implements Converter<MembershipEntity, Membership> {
 
 	private UserEntityConverter userEntityConverter = new UserEntityConverter();
